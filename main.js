ca338405811d6559fa781cfd4454e9e2e6d9e4e4
Aldona Chudzik <aldonachudzik@gmail.com>
2020-07-24 18:29:39 +0200

    zmianaWygladuKodu

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert('Siema!');
+const name = 'Aldona';
+const age = 32;
+alert('ok');
+console.log(`Cześć, nazywam się ${name} i mam ${age} lata.`)
+const heading = document.querySelector('.main-heading--js');
+console.log(heading);
+heading.innerHTML = `zmieniony tekst`
